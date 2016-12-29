605abe332a810c6c1d9ba373e454c226025d9015
maimous@gmail.com <stefaleon@gmail.com>
2016-12-29 11:08:31 +0200

    fix localhost problem after fixing https for open weather api

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const PORT = process.env.PORT || 3000;
 
 // redirect https traffic to http, due to open weather api restriction
 app.use(function (req, res, next) {
-	if (req.headers['x-forwarded-proto'] === 'http') {
-		next();
-	} else {
+	if (req.headers['x-forwarded-proto'] === 'https') {
 		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
 	}
 });
 
